baa8d9673debad3bfa08966f05c56f27fcbf5fd2
Ruslan Gajiev <62586375+RuslanGajiev@users.noreply.github.com>
2020-08-28 02:18:48 +0700

    Bug fix

--- state_machine/src/stdin.c
+++ state_machine/src/stdin.c
@@ -1,83 +1,81 @@
 #include "references.h"
 #include "all_structs.h"
 
-void modification(unsigned char *buffer,const unsigned int lenght,unsigned char **arg_vector){
-        unsigned char **arg_vctr = arg_vector;
-        const unsigned int packet_len = lenght;
-        char c[2];
-        unsigned char *packet_buffer = buffer;
-        scanf("%1s",c);
-        if (c[0] == 'n' ){
-                printf("\n\n");
-                return;
+/*State-Machine*/
+
+int main(int argc,unsigned char *argv[]){
+        char *dev, errbuf[PCAP_ERRBUF_SIZE];
+        dev = pcap_lookupdev(errbuf);
+        if (dev == NULL) {
+                fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+                return(2);
+        }
+        printf("Sniffing on device: %s\n", dev);
+
+        unsigned char **arg_vector = argv;
+        int raw_sock_all = socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+        if (raw_sock_all == -1){
+                printf("%s", "ERROR_ALL\n");
+                exit(SADNESS); 
         }
 
-        printf("Choose: {ARP}, {ICMP}(Only payload), {IP}, {UDP}, {TCP}\n");
-        char protocol[10];
-        char *ptr= protocol;
-        scanf("%s", ptr);
-        IP_HDR *ip_header;
-        ip_header = (IP_HDR *)buffer;
-        ip_header = (IP_HDR *)(buffer + ETHER_HEAD_LEN);
-        ETHER_HDR *ether_header;
-        ether_header = (ETHER_HDR *)buffer;
-        if(strcmp(ptr,"ARP") == 0){
-                if(ntohs(ether_header->ether_type) == ARP){
-                        arp_proto_modify(buffer + ETHER_HEAD_LEN);
-        }     
-                else{
-                        printf("\n[FAILURE]\n\n");
-                        return ;
-                }   
-        }
-        if(strcmp(ptr,"IP") == 0){
-                if(ntohs(ether_header->ether_type) == IPv4){ 
-                        ip_proto_modify(buffer + ETHER_HEAD_LEN);
-                }
+        int raw_sock = socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+        if (raw_sock == -1){
+                printf("%s","ERROR_RAW\n");
+                exit(SADNESS);
+        } 
 
-                else {
-                        printf("\n[FAILURE]\n\n");
-                        return ;
-                }   
-        }
-        if(strcmp(ptr,"ICMP") == 0){
-                if(ip_header->ip_type_prot == ICMP){
-                        icmp_proto_modify(packet_buffer,packet_len,arg_vctr);
-                }
- 
-                else{
-                        printf("\n[FAILURE]\n\n");
-                        return ;
-                }   
-        }
-        if(strcmp(ptr,"UDP") == 0){
-                if(ip_header->ip_type_prot == UDP){
-                        udp_proto_modify(buffer + ETHER_HEAD_LEN + sizeof(IP_HDR));
+        enum ARP_IP{
+        ARP = 0x0806,
+        IP4 = 0x0800,
+        IP = 0,
+        ICMP = 1,
+        IGMP = 2,
+        TCP = 6,
+        UDP = 17
+        };
+
+        unsigned char buffer[DATA_BUFFER_SIZE] = {0};
+        for(int i=0;i<5;i++){
+                int pack_size=recv(raw_sock_all,buffer,1024,0);
+                ETHER_HDR *ether_ptr;
+                ether_ptr = (ETHER_HDR *)buffer;
+                IP_HDR *ip_ptr;
+                ip_ptr = (IP_HDR *)buffer;
+                ip_ptr = (IP_HDR *)(buffer + ETHER_HEAD_LEN);
+                switch(ntohs(ether_ptr->ether_type)){
+                        case ARP:
+                                STATE_ARP(buffer,pack_size,raw_sock);
+                                break;
+                        
+                        case IP4:
+                                switch(ip_ptr->ip_type_prot){
+                                        case IP:
+                                                STATE_IP(buffer,pack_size,raw_sock);
+                                                break;
+
+                                        case ICMP:
+                                                STATE_ICMP(buffer,pack_size,raw_sock,arg_vector);
+                                                break;
+
+                                        case TCP:
+                                                STATE_TCP(buffer,pack_size,raw_sock);
+                                                break;
+
+                                        case UDP:
+                                                STATE_UDP(buffer,pack_size,raw_sock);
+                                                break;
+
+                                        case IGMP:
+                                                STATE_IGMP(buffer,pack_size,raw_sock);
+                                                break;
+                                        default:
+                                                break;
+                                }
+                                break;
+
+                        default:
+                                break;
                 } 
- 
-                else {
-                        printf("\n[FAILURE]\n\n");
-                        return ;
-                }   
         }
-        if(strcmp(ptr,"TCP") == 0){
-                if(ip_header->ip_type_prot == TCP){
-                        tcp_proto_modify(buffer + ETHER_HEAD_LEN + sizeof(IP_HDR));
-                }
- 
-                else{
-                        printf("\n[FAILURE]\n\n");
-                        return ;
-                }   
-        }
-}
- 
-void if_have_tcp_udp(const unsigned char *buffer){
-        IP_HDR *ip_header;
-        ip_header = (IP_HDR *)(buffer + ETHER_HEAD_LEN);
-        if(ip_header->ip_type_prot == UDP)
-                udp_proto_modify(buffer + ETHER_HEAD_LEN + sizeof(IP_HDR));
-
-        if(ip_header->ip_type_prot == TCP)
-                tcp_proto_modify(buffer + ETHER_HEAD_LEN + sizeof(IP_HDR));
 }
