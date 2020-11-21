d667710be06a64c0c3649586c9b0d01ba963e1d7
ruslangajiev <ruslangajiev9@gmail.com>
2020-11-21 19:45:30 +0700

    fixes

--- state_machine/src/dataHandlers.c
+++ state_machine/src/dataHandlers.c
@@ -4,22 +4,20 @@
 void dumptraffic(const unsigned char *data_buffer, const unsigned int length) {
     unsigned char byte;
     unsigned int i,j;
-    for(i=0; i<length;i++){
-          byte=data_buffer[i];
-          printf("%02x ",data_buffer[i]);
-          if (((i%16)==15) || (i==length-1)){  
-                printf(" ");
-                printf("| ");
-                for(j=(i-(i%16));j<=i;j++){
-                        byte=data_buffer[j];
-                        if ((byte>31)&&(byte<127)) 
-                        printf("%c",byte);
-                        else
-
-                        printf("."); 
-                }
-                        printf("\n");
-          }
+    for(i = 0; i < length; i++){
+        byte=data_buffer[i];
+        printf("%02x ", byte);
+        if (((i%16) == 15) || (i == length - 1)){
+            printf(" | ");
+            for(j = (i - (i%16)); j <= i; j++){
+                byte=data_buffer[j];
+                if ((byte>31)&&(byte<127))
+                    printf("%c",byte);
+                else
+                    printf(".");
+            }
+            printf("\n");
+        }
     }
 }
 
